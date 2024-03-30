8f8814e106f7d068eaefe7105e3efef13f917d0a
J-shw <91017208+J-shw@users.noreply.github.com>
2024-03-30 18:28:51 +0000

    Update startup.py

--- modules/startup.py
+++ modules/startup.py
@@ -1,23 +1,7 @@
 import os
 
 # Setup initial empty folders if not present
-folders = ['static/data/photos/HR', 'static/data/photos/LR'] 
+folders = ['static/data/photos/HR', 'static/data/photos/LR', 'static/data/logs'] 
 for folder in folders:
     if not os.path.exists(folder):
         os.mkdir(folder)
-
-from modules.sysLogger import logger # Must be imported after folder creation
-# Clear temp folder
-try:
-    directory_path = 'static/data/temp/'
-    # List all files in the directory
-    files = os.listdir(directory_path)
-
-    # Iterate over each file and remove it
-    for file in files:
-        file_path = os.path.join(directory_path, file)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-
-except Exception as e:
-    logger.error(f"Failed to clean temp folder: {e}")
