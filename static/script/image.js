b55e5330e7023fa1ebeca1c0e6d5e040c79f90f0
J-shw <91017208+J-shw@users.noreply.github.com>
2024-04-02 09:14:48 +0100

    Created new functions - goHome and downloadImage

--- static/script/image.js
+++ static/script/image.js
@@ -1,5 +1,4 @@
 function deleteImage(imagePath){
-
     if (window.confirm("Are you sure you want to delete this item?")) {
 
         fetch('/delete_image/'+imagePath)
@@ -17,4 +16,34 @@ function deleteImage(imagePath){
         })
       }
 
+}
+
+function goHome() {
+    window.location.href = '/';
+}
+
+async function downloadImage(imagePath) {
+    try {
+      const response = await fetch(`/download/image/${imagePath}`, { method: 'GET' });
+  
+      if (response.ok) {
+        const blob = await response.blob();
+  
+        // Create a temporary link element
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = imageName;
+  
+        // Append the link to the body and click it programmatically
+        document.body.appendChild(link);
+        link.click();
+  
+        // Clean up: remove the temporary link element
+        document.body.removeChild(link);
+      } else {
+        console.error('Download failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Download error:', error);
+    }
 }
