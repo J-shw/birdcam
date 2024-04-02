dca86e0b4bb51aa02874dd196af7010ed250838f
Josh <91017208+J-shw@users.noreply.github.com>
2024-04-02 17:49:23 +0100

    Fixed startup

--- modules/startup.py
+++ modules/startup.py
@@ -1,7 +1,7 @@
 import os
 
 # Setup initial empty folders if not present
-folders = ['static/data/photos/HR', 'static/data/photos/LR', 'static/data/logs'] 
+folders = ['static/data/logs', 'static/data/photos', 'static/data/photos/HR', 'static/data/photos/LR'] 
 for folder in folders:
     if not os.path.exists(folder):
         os.mkdir(folder)
