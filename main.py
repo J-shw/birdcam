67d4fd6b9a8dcc317735fe6bdd13d94143c09af5
Josh <91017208+J-shw@users.noreply.github.com>
2024-04-24 19:18:50 +0100

    Now stores the files as dict instead of array

--- main.py
+++ main.py
@@ -88,18 +88,22 @@ def lowResImg_loc():
         files_and_directories = os.listdir(lowResPath)
         directories = [d for d in files_and_directories if os.path.isdir(os.path.join(lowResPath, d))]
         for d in directories:
-            current = []
             current.append(d)
             newFilePath = str(lowResPath) + str(d)
             files_and_directories = os.listdir(newFilePath)
             files = [f for f in files_and_directories if os.path.isfile(os.path.join(newFilePath, f))]
             
-            if len(files) == 0: return(jsonify(status=404, data="No files found"))
-            for f in files:
-                current.append(f)
+            if len(files) != 0: 
+                files_list = []
+                for f in files:
+                    files_list.append(f)
+                
+                current = {
+                    "folder": d,
+                    "files": files_list
+                }
 
-            sorted_current = [current[0]] + sorted(current[1:], key=lambda x: x.split('.')[0], reverse=True)
-            storage.append(sorted_current)
+                storage.append(current)
     except Exception as e:
         logger.error(e)
         return(jsonify(status=500, data=str(e)))
